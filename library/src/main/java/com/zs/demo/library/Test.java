7494bc12f7f2311a907145eebb4eb5334648d10c
CodeZhang <552146383@qq.com>
2018-11-28 10:32:16 +0800

    test

---
+++ library/src/main/java/com/zs/demo/library/Test.java
@@ -0,0 +1,18 @@
+package com.zs.demo.library;
+
+/**
+ * Created by zs
+ * Date：2018年 11月 28日
+ * Time：10:20
+ * —————————————————————————————————————
+ * About:
+ * —————————————————————————————————————
+ */
+public class Test {
+
+    public int addAction(int a , int b){
+        return a + b;
+    }
+
+
+}
